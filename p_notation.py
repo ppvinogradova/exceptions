09aba649cec067bbfd2946b7385a81770775ce46
Polina Vinogradova <p.p.vinogradova@yandex.ru>
2020-02-09 16:45:40 +0200

    no side error output

--- p_notation.py
+++ p_notation.py
@@ -1,25 +1,12 @@
+import sys
 notation = str(input('Введите польскую нотацию двух положительных чисел '))
 notation_list = notation.split()
 
 class InvalidInputError(Exception):
     def __init__(self, text):
         self.text = text
-    
-try:
-    first_num = notation_list[1]
-    operation = notation_list[0]
-    second_num = notation_list[2]
-    if len(notation_list) > 3:
-        raise InvalidInputError('Введено более 2х цифр')
-except IndexError:
-    print('Введено 1 число')
-except InvalidInputError as bar:
-    print(bar)
-
-available_op_list = ['+', '-', '*', '/']
-assert operation in available_op_list , 'Вы ввели неверный знак'
-
-class polish_num():
+        
+class PolishNum():
     def __init__(self, num):
         self.num = num
 
@@ -33,17 +20,36 @@ class polish_num():
         return other * self.num
     
     def __truediv__(self, other):
-        return other / self.num
-    
-first = polish_num(first_num)
-second = polish_num(second_num)
+        return other / self.num  
+
+
+try:
+    first_num = notation_list[1]
+    operation = notation_list[0]
+    second_num = notation_list[2]
+    if len(notation_list) > 3:
+        raise InvalidInputError('Некорректный ввод. Введите знак и 2 числа')
+    available_op_list = ['+', '-', '*', '/']
+    assert operation in available_op_list , 'Вы ввели неверный знак'
+except IndexError:
+    print('Введено 1 число')
+    sys.exit()
+except InvalidInputError as bar:
+    print(bar)
+    sys.exit()
+except AssertionError as ae:
+    print(ae)
+    sys.exit()
+
+first = PolishNum(first_num)
+second = PolishNum(second_num)
 
 try:
     a = int(first.num)
     b = int(second.num)
 except ValueError:
     print('Введено неправильное значение')
-
+    sys.exit()
 if operation == '+':
     sum = a + b
     print(sum)
@@ -59,8 +65,4 @@ elif operation == '/':
       print(div)
     except ZeroDivisionError:
         print('Деление на ноль')
-        div = a
-        print(a)
-    except NameError:
-        div = 0
-        print(div)
+        
